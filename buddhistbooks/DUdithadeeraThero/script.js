21941d02eca860fb6cb4a47d25d104bfea4c858d
nipunadhamma <kalawanesangamaji@gmail.com>
2026-07-05 18:34:38 +0530

    Update script.js

--- buddhistbooks/DUdithadeeraThero/script.js
+++ buddhistbooks/DUdithadeeraThero/script.js
@@ -1,14 +1,65 @@
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        const listContainer = document.getElementById('file-list');
-        data.forEach(file => {
-            const div = document.createElement('div');
-            div.className = 'item';
-            div.innerHTML = `
-                <span>${file.name} (${file.size})</span>
-                <a href="${file.name}" download>බාගත කරන්න</a>
-            `;
-            listContainer.appendChild(div);
-        });
-    });
+fetch("data.json")
+
+.then(r=>r.json())
+
+.then(data=>{
+
+const list=document.getElementById("bookList");
+
+data.forEach(book=>{
+
+const div=document.createElement("div");
+
+div.className="book";
+
+div.innerHTML=`
+
+<div class="left">
+
+<span class="material-icons">
+
+picture_as_pdf
+
+</span>
+
+<span class="title">
+
+${book.title}
+
+</span>
+
+</div>
+
+<div class="buttons">
+
+<button class="view">View</button>
+
+<button class="download">Download</button>
+
+</div>
+
+`;
+
+div.querySelector(".view").onclick=()=>{
+
+window.open(book.file,"_blank");
+
+};
+
+div.querySelector(".download").onclick=()=>{
+
+const a=document.createElement("a");
+
+a.href=book.file;
+
+a.download="";
+
+a.click();
+
+};
+
+list.appendChild(div);
+
+});
+
+});
