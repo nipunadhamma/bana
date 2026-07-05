c0ec638a3fd5557bf038e0d6e3c31e0f02375e37
nipunadhamma <kalawanesangamaji@gmail.com>
2026-07-05 18:21:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const list = document.getElementById('list');
+fetch("Folderdata.json")
 
-    // පරීක්ෂා කිරීම: list එක අඩවියේ තිබේද?
-    if (!list) {
-        console.error("Error: 'list' id එක HTML එකේ හොයාගන්න බැහැ.");
-        return;
-    }
+.then(response=>response.json())
 
-    fetch('Folderdata.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("JSON ගොනුව කියවීමේ දෝෂයක්: " + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            list.innerHTML = ""; // පරණ දේවල් මකන්න
-            data.forEach(item => {
-                const div = document.createElement('div');
-                div.className = 'item';
-                div.innerHTML = `📁 <a href="${item.path}">${item.name}</a>`;
-                list.appendChild(div);
-            });
-        })
-        .catch(error => {
-            console.error("වැරැද්දක් සිදුවුණා:", error);
-            list.innerHTML = "<p>දත්ත පූරණය කිරීමේ දෝෂයක් සිදුවිය.</p>";
-        });
+.then(data=>{
+
+const list=document.getElementById("authorList");
+
+data.forEach(author=>{
+
+const card=document.createElement("div");
+
+card.className="card";
+
+card.innerHTML=`
+
+<div class="left">
+
+<span class="material-icons">
+
+folder
+
+</span>
+
+<span class="name">
+
+${author.name}
+
+</span>
+
+</div>
+
+<span class="material-icons arrow">
+
+chevron_right
+
+</span>
+
+`;
+
+card.onclick=()=>{
+
+location.href=author.folder+"index.html";
+
+};
+
+list.appendChild(card);
+
+});
+
 });
