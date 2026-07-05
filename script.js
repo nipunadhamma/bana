3884a12bbb4c133f20c784184ca47083bc407861
nipunadhamma <kalawanesangamaji@gmail.com>
2026-07-05 14:22:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-// මුල් පිටුවේ ෆෝල්ඩර පෙන්වීමට
-function loadFolders() {
-    fetch('Folderdata.json')
-        .then(res => res.json())
-        .then(data => {
-            const list = document.getElementById('list');
-            Object.keys(data).forEach(author => {
-                list.innerHTML += `<div class="item" onclick="showBooks('${author}')">📁 ${author}</div>`;
-            });
+fetch('Folderdata.json')
+    .then(res => res.json())
+    .then(data => {
+        const list = document.getElementById('list');
+        data.forEach(item => {
+            // කතුවරයාගේ නම ක්ලික් කළ විට අදාළ index.html එක විවෘත වේ
+            list.innerHTML += `<div class="item">📁 <a href="${item.path}">${item.name}</a></div>`;
         });
-}
-
-// පොත් ලැයිස්තුව පෙන්වීමට
-function showBooks(author) {
-    fetch('Folderdata.json')
-        .then(res => res.json())
-        .then(data => {
-            const list = document.getElementById('list');
-            list.innerHTML = `<button onclick="location.reload()">ආපසු</button>`; // මුල් පිටුවට යාමට
-            data[author].forEach(book => {
-                list.innerHTML += `<div class="item">📄 ${book.name} <a href="${book.file}" download>Download</a></div>`;
-            });
-        });
-}
+    });
