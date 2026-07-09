f639f15b20867802f476606a021163179bed1034
nipunadhamma <kalawanesangamaji@gmail.com>
2026-07-09 19:34:26 +0530

    Update book.js

--- books/common/book.js
+++ books/common/book.js
@@ -19,10 +19,18 @@ $(document).keydown(function (e) {
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
-const clipb = new ClipboardJS('.share-icon', { //.TOC-text .share-icon
+const clipb = new ClipboardJS('.share-icon', {
     text: function (icon) {
-        const bookFolder = $(icon).parents('[book-folder]:first').attr('book-folder');
-        return `https://books.nipunadhamma.org/books/${bookFolder}/${$(icon).attr('file-name') || ''}`;
+
+        const bookmarkData =
+            JSON.parse(
+                icon.parentElement.querySelector('.star-icon')
+                    .dataset.bookmark
+            );
+
+        const bookFolder = bookmarkData.book.folder;
+
+        return `https://books.nipunadhamma.org/books/${bookFolder}/${icon.getAttribute('file-name')}`;
     }
 });
 clipb.on('success', e => showToast('link එක copy කර ගත්තා. ඔබට අවශ්‍ය තැන paste කරන්න.'));
